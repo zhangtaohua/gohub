e3defe61960540ea2d42ac5a83b01e841453e360
RJ <zthvivid@163.com>
2022-05-08 18:29:36 +0800

    创建分类

---
+++ app/http/controllers/api/v1/categories_controller.go
@@ -0,0 +1,101 @@
+package v1
+
+import (
+	"github.com/zhangtaohua/gohub/app/models/category"
+	"github.com/zhangtaohua/gohub/app/requests"
+	"github.com/zhangtaohua/gohub/pkg/response"
+
+	"github.com/gin-gonic/gin"
+)
+
+type CategoriesController struct {
+	BaseAPIController
+}
+
+// func (ctrl *CategoriesController) Index(c *gin.Context) {
+//     categories := category.All()
+//     response.Data(c, categories)
+// }
+
+// func (ctrl *CategoriesController) Show(c *gin.Context) {
+//     categoryModel := category.Get(c.Param("id"))
+//     if categoryModel.ID == 0 {
+//         response.Abort404(c)
+//         return
+//     }
+//     response.Data(c, categoryModel)
+// }
+
+func (ctrl *CategoriesController) Store(c *gin.Context) {
+
+	request := requests.CategoryRequest{}
+	if ok := requests.Validate(c, &request, requests.CategorySave); !ok {
+		return
+	}
+
+	categoryModel := category.Category{
+		Name:        request.Name,
+		Description: request.Description,
+	}
+
+	categoryModel.Create()
+	if categoryModel.ID > 0 {
+		response.Created(c, categoryModel)
+	} else {
+		response.Abort500(c, "创建失败，请稍后尝试~")
+	}
+}
+
+// func (ctrl *CategoriesController) Update(c *gin.Context) {
+
+//     categoryModel := category.Get(c.Param("id"))
+//     if categoryModel.ID == 0 {
+//         response.Abort404(c)
+//         return
+//     }
+
+//     if ok := policies.CanModifyCategory(c, categoryModel); !ok {
+//         response.Abort403(c)
+//         return
+//     }
+
+//     request := requests.CategoryRequest{}
+//     bindOk, errs := requests.Validate(c, &request, requests.CategorySave)
+//     if !bindOk {
+//         return
+//     }
+//     if len(errs) > 0 {
+//         response.ValidationError(c, 20101, errs)
+//         return
+//     }
+
+//     categoryModel.FieldName = request.FieldName
+//     rowsAffected := categoryModel.Save()
+//     if rowsAffected > 0 {
+//         response.Data(c, categoryModel)
+//     } else {
+//         response.Abort500(c, "更新失败，请稍后尝试~")
+//     }
+// }
+
+// func (ctrl *CategoriesController) Delete(c *gin.Context) {
+
+//     categoryModel := category.Get(c.Param("id"))
+//     if categoryModel.ID == 0 {
+//         response.Abort404(c)
+//         return
+//     }
+
+//     if ok := policies.CanModifyCategory(c, categoryModel); !ok {
+//         response.Abort403(c)
+//         return
+//     }
+
+//     rowsAffected := categoryModel.Delete()
+//     if rowsAffected > 0 {
+//         response.Success(c)
+//         return
+//     }
+
+//     response.Abort500(c, "删除失败，请稍后尝试~")
+// }
--- routes/api.go
+++ routes/api.go
@@ -53,12 +53,17 @@ func RegisterAPIRoutes(r *gin.Engine) {
 		}
 
 		uc := new(controllers.UsersController)
-
 		// 获取当前用户
 		v1.GET("/user", middlewares.AuthJWT(), uc.CurrentUser)
 		usersGroup := v1.Group("/users")
 		{
 			usersGroup.GET("", uc.Index)
+		}
+
+		cgc := new(controllers.CategoriesController)
+		cgcGroup := v1.Group("/categories")
+		{
+			cgcGroup.POST("", middlewares.AuthJWT(), cgc.Store)
 		}
 	}
 }
