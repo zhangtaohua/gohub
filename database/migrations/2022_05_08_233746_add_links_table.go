a03b0ec671446d0a882fea2fb52443605b1527b4
RJ <zthvivid@163.com>
2022-05-08 23:40:06 +0800

    友情链接模型和迁移

---
+++ database/migrations/2022_05_08_233746_add_links_table.go
@@ -0,0 +1,32 @@
+package migrations
+
+import (
+	"database/sql"
+
+	"github.com/zhangtaohua/gohub/app/models"
+	"github.com/zhangtaohua/gohub/pkg/migrate"
+
+	"gorm.io/gorm"
+)
+
+func init() {
+
+	type Link struct {
+		models.BaseModel
+
+		Name string `gorm:"type:varchar(255);not null"`
+		URL  string `gorm:"type:varchar(255);default:null"`
+
+		models.CommonTimestampsField
+	}
+
+	up := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.AutoMigrate(&Link{})
+	}
+
+	down := func(migrator gorm.Migrator, DB *sql.DB) {
+		migrator.DropTable(&Link{})
+	}
+
+	migrate.Add("2022_05_08_233746_add_links_table", up, down)
+}
